33d2226de551d1efdd22f2bd28f46bc3f0076a20
FLEYreal <borisov.nikita.off@gmail.com>
2023-12-30 20:12:17 +0800

    Socks

---
+++ socks/client.py
@@ -0,0 +1,6 @@
+
+class Client:
+    """Class of methods to work with sockets on client side"""
+
+    def __init__(self) -> None:
+        pass
---
+++ socks/server.py
@@ -0,0 +1,6 @@
+
+class Server:
+    """Class of methods to work with sockets on server side"""
+
+    def __init__(self) -> None:
+        pass
