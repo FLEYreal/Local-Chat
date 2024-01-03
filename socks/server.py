1027b85718484d6bdaffa49e459a51e56cab2d69
FLEYreal <borisov.nikita.off@gmail.com>
2024-01-03 13:44:00 +0800

    Server side changed

--- socks/server.py
+++ socks/server.py
@@ -23,9 +23,9 @@ class Server:
 
     def __init__(
             self,
-            username = "Guest",
-            host = ('0.0.0.0', 5005),
-            header_length = 16
+            username="Guest",
+            host=('0.0.0.0', 5005),
+            header_length=16
     ) -> None:
         self.username = username
         self.host = host
@@ -58,11 +58,11 @@ class Server:
             # Loop to listen to connection
             if not return_server:
                 self.listen(server)
-            
+
             # Return server if you need to make your own functionality with it
             elif return_server:
                 return server
-        
+
         except Exception as e:
             log.error(f'Error on Server.create() : {e}')
             return False
@@ -87,7 +87,7 @@ class Server:
             # If no header
             if not len(header):
                 raise Exception('Feels like nothing was sent in the header!')
-            
+
             # Get provided length
             length = int(header.decode('UTF-8').strip())
 
@@ -98,7 +98,7 @@ class Server:
                 'header': header,
                 'data': data
             }
-        
+
         except Exception as e:
             log.error(f'Error on Server.receive() : {e}')
             return False
@@ -116,12 +116,14 @@ class Server:
         try:
 
             if server == False:
-                raise Exception('Provided server equals "False" which most times implies that error happened on Server.create(). It also means that Server.listen() returns False as well!')
+                raise Exception(
+                    'Provided server equals "False" which most times implies that error happened on Server.create(). It also means that Server.listen() returns False as well!')
 
             while True:
 
                 # Create waiting list
-                rlist, _, xlist = select.select(self.sockets_list, [], self.sockets_list)
+                rlist, _, xlist = select.select(
+                    self.sockets_list, [], self.sockets_list)
 
                 # Parse ready list
                 for _socket in rlist:
@@ -139,12 +141,33 @@ class Server:
                         # Save data
                         self.sockets_list.append(client)
                         self.clients_dict[client] = response
-                        
-                        # Get data from response
-                        header, data = response
 
-                        if data["type"] == 'send' or data["type"] == 'leave':
-                            print(data['message'])
+                        # Get data from response
+                        data = response['data']
+
+                        if data["type"] == 'join' or data["type"] == 'leave':
+                            print(f"({address[0]}) " + data['message'])
+
+                        elif data["type"] == 'send':
+                            print(f'{self.username} {Fore.BLACK}:{Fore.RESET} {data["message"]}')
+
+                    else:
+
+                        # Receive user's data
+                        response = self.receive(client)
+                        if not response:
+                            print(f"Connection from ({address[0]}) has been interrupted!")
+
+                            self.sockets_list.remove(_socket)
+                            del self.clients_dict[_socket]
+
+                            continue
+
+                        # Get data from response
+                        data = response['data']
+
+                        if data["type"] == 'join' or data["type"] == 'leave':
+                            print(f"({address[0]}) " + data['message'])
 
                         elif data["type"] == 'send':
                             print(f'{self.username} {Fore.BLACK}:{Fore.RESET} {data["message"]}')
